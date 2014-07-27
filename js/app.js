ca233f02a41a3ff7ba29af0fe43328cffbfb082c
alex <googirard.alex@gmail.com>
2014-07-27 11:19:53 +1000

    top

--- js/app.js
+++ js/app.js
@@ -3,9 +3,56 @@
 
   var module = angular.module('myApp', ['onsen', 'ui.map']);
 
-  module.run(['$rootScope', function($rootScope){
-    $rootScope.state = {};
+  // module.config(['$httpProvider', function($httpProvider) {
+  //     $httpProvider.defaults.useXDomain = true;
+  //     delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  //   }
+  // ]);
+
+  module.run(['$rootScope', function($rootScope, $httpProvider){
+    $rootScope.state = {
+      placeholders: {
+        'carrots': 'img/vegetables/carrots.jpg',
+        'broccoli-greens': 'img/vegetables/broccoli-greens.jpg',
+        'broccoli-head': 'img/vegetables/broccoli-head.jpg',
+        'eggplant-line-up': 'img/vegetables/eggplant-line-up.jpg',
+        'poivron': 'img/vegetables/poivron.jpg',
+        'potatoes': 'img/vegetables/potatoes.jpg'
+      },
+      icons: {
+        'carrots': 'img/icons/icons-sliced_34.png',
+        'broccoli-greens': 'img/icons/icons-sliced_32.png',
+        'broccoli-head': 'img/icons/icons-sliced_40.png',
+        'eggplant-line-up': 'img/icons/icons-sliced_23.png',
+        'poivron': 'img/icons/icons-sliced_25.png',
+        'potatoes': 'img/icons/icons-sliced_26.png'
+      }
+    };
   }]);
+
+  module.controller('MenuController', function($scope, $timeout, $rootScope, $data){
+    $scope.spin = false;
+
+    $scope.auth = function(){
+      console.log('SPIN');
+      $scope.spin = true;
+      $timeout(function(){
+        $scope.spin = false;
+        $rootScope.state.user = {
+          id: '003',
+          name: 'Alex',
+          role: 'seller'
+        };
+      },1300);
+    };
+
+    $scope.goto = function(n){
+      $rootScope.state.farmer = $data.farmers[$rootScope.state.user.id];
+      $rootScope.state.items = $data.items[$rootScope.state.user.id];
+      $scope.menu.setAbovePage('tpl/crop.html', {closeMenu: true})
+    };
+
+  })
 
   module.controller('MapCtrl', function($scope, $data, $timeout, $rootScope, $http) {
     
@@ -13,7 +60,7 @@
 
     $scope.mapOptions = {
       center: currentLatLng,
-      zoom: 15,
+      zoom: 14,
       disableDefaultUI: true,
       styles: [{
         featureType: "poi",
@@ -32,7 +79,7 @@
           position: new google.maps.LatLng(farmer.position.lat, farmer.position.lng),
           icon: {
               path: fontawesome.markers.MAP_MARKER,
-              scale: 0.6,
+              scale: 0.7,
               strokeWeight: 0.9,
               strokeColor: 'black',
               strokeOpacity: 1,
@@ -56,6 +103,7 @@
 
     $timeout(function(){
       google.maps.event.trigger($scope.myMap, 'resize');
+      $scope.myMap.panTo(currentLatLng);
     }, 300);
 
     $scope.onMapIdle = function(){
@@ -68,11 +116,19 @@
     //   $scope.ons.slidingMenu.setMainPage('tpl/producer.html');
     // }, 1000);
 
-    $http.get('http://shrouded-lowlands-4710.herokuapp.com/products.json')
+    // $http.get('http://shrouded-lowlands-4710.herokuapp.com/')
+    $http.get('https://43e6a5d2.ngrok.com/')
     .success(function(){
       console.log('OK', arguments);
     });
 
+  })
+
+  module.controller('MarketController', function($scope, $rootScope){
+
+    $scope.remove = function(id){
+      console.log('@@ remove', id);
+    };
   })
 
   module.controller('ShopController', function($scope, $data, $rootScope) {
@@ -111,7 +167,8 @@
       console.log('@@@@@ signin');
       $rootScope.state.user = {
         name: 'Alex',
-        role: 'user'
+        role: 'user',
+        id: '003'
       };
       $rootScope.state.needAuth = false;
       //utf8=%E2%9C%93&authenticity_token=wrP8fuZ6QFUff7pJ%2B7vdEx%2BlHhP1ZeGaZ3ranPJXYkU%3D&user%5Busername%5D=alex&user%5Bemail%5D=googirard.alex%40gmail.com&user%5Bpassword%5D=12345&user%5Bpassword_confirmation%5D=12345&commit=Sign+Up
@@ -174,15 +231,19 @@
       data.items = {
         '001': {
           '001':{id:'001', sort: 'carrots', price: 2, image: 'img/item.001.jpg'},
-          '002':{id:'002', sort: 'broccoli', price: 3, image: 'img/item.002.jpg'},
-          '003':{id:'003', sort: 'eggs', price: 6, image: 'img/item.003.jpg'},
-          '004':{id:'004', sort: 'apples', price: 5, image: 'img/item.004.jpg'},
-          '005':{id:'005', sort: 'tomatoes', price: 4, image: 'img/item.005.jpg'}
+          '002':{id:'002', sort: 'broccoli-greens', price: 3, image: 'img/item.002.jpg'},
+          '003':{id:'003', sort: 'potatoes', price: 6, image: 'img/item.003.jpg'},
+          '004':{id:'004', sort: 'eggplant-line-up', price: 5, image: 'img/item.004.jpg'},
+          '005':{id:'005', sort: 'poivron', price: 4, image: 'img/item.005.jpg'}
         },
         '002': {
-          '006':{id:'006', sort: 'eggplant', price: 2, image: 'img/item.006.jpg'},
-          '007':{id:'007', sort: 'eggs', price: 5, image: 'img/item.007.jpg'},
-          '008':{id:'008', sort: 'cheries', price: 3, image: 'img/item.008.jpg'}
+          '006':{id:'006', sort: 'eggplant-line-up', price: 2, image: 'img/item.006.jpg'},
+          '007':{id:'007', sort: 'broccoli-head', price: 5, image: 'img/item.007.jpg'},
+          '008':{id:'008', sort: 'poivron', price: 3, image: 'img/item.008.jpg'}
+        },
+        '003': {
+          '009':{id:'009', sort: 'eggplant-line-up', price: 2, image: 'img/item.004.jpg'},
+          '010':{id:'010', sort: 'potatoes', price: 7, image: 'img/item.003.jpg'}
         }
       };
 
